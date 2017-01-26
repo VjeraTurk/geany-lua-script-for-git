b8faaac0bb3061f4045a8cf0277075141d822ae4
Goran <ferarista@live.com>
2017-01-26 21:00:10 +0100

    no comment

--- commit.lua
+++ commit.lua
@@ -172,11 +172,11 @@ end
 				dialog.label(dialog, "Pick files to add")
 				
 					for i,file in ipairs(files) do
-						file[i] = dialog.checkbox ( dialog,"files", false, files[i])
+						dialog.checkbox ( dialog,"files", false, files[i])
 			end
 			
 			result = dialog.run(dialog)
-			geany.message(result)
+			geany.message(result[1])
 			
 		end
 	end
