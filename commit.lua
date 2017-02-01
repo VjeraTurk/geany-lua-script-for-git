4cf75ed703ccb13428815245e942848c3f2fecfa
VjeraTurk <vjera.turk@gmail.com>
2017-02-01 19:26:49 +0100

    Test

--- commit.lua
+++ commit.lua
@@ -19,7 +19,7 @@ function scandir(directory)
     pfile:close()
     return t
 end
--- Funtion displays all files in current file folder and checkbox for each , calls git add for each checked files
+-- Funtion displays all files in argument folder and checkbox for each one, calls git add for each checked files
 function addFiles(path)
 
 	local files = scandir(path)
@@ -159,10 +159,15 @@ if result=="fatal: Not a git repository (or any of the parent directories): .git
 
 end
 
+function commit(message)
+
+	
+end
+
 if result==''  then
+
 	geany.banner = "Commit your changes"
 	message=geany.input("Commit message", "no comment")
-
 	if message ~= nil then
 		cmd="cd "..FILE_DIR_PATH.."  2>&1\n git commit -m \""..message.."\""
 	else
@@ -180,7 +185,14 @@ if result==''  then
 	choice = geany.confirm("Add untracked files to repository"  ,"Add untracked files to your repository?",true)
 
 		if choice == true then
-		addFiles(FILE_DIR_PATH)
+			addFiles(FILE_DIR_PATH)
+			geany.banner = "Commit your changes"
+			message=geany.input("Commit message", "added untracked files")
+				if message ~= nil then
+					cmd="cd "..FILE_DIR_PATH.."  2>&1\n git commit -m \""..message.."\""
+				else
+					cmd="cd "..FILE_DIR_PATH.."  2>&1\n git commit -m \"no comment\""
+				end
 		end
 
 	end
