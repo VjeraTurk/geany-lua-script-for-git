74f7d4c207514e43bfaed2e4800aa41885d7947a
VjeraTurk <vjera.turk@gmail.com>
2017-02-02 18:10:14 +0100

    no comment

--- commit.lua
+++ commit.lua
@@ -102,7 +102,7 @@ cmds={
 	result = handle:read("*a")
 	handle:close()
 	
-	geany.message(" "..cmd.." :\n"..result.."")
+	geany.message(" "..cmd.." :\n"..result)
 
 		if string.match(result,"koko: command not found") then
 			install_msg="Before you start using Git, you have to make it available on your computer. You can either install it as a package or via another installer, or download the source code and compile it yourself. \nDebian/Ubuntu:\n$ yum install git \nFedora:\n $ yum install git"
